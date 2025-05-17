eeeed67a3bc123cedc9e29001c40a09c8aeeef03
rohitsaini00140 <rohit@hhpsoftware.in>
2025-05-17 12:10:21 +0530

    fixed Text

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -64,7 +64,7 @@ const Team = () => {
     <section className="bg-white-300 lg:py-10 relative z-10 py-3 dark:bg-bg-color-dark md:py-16">
       <div className="container">
         <SectionTitle
-          title="TEAM MEMBERS"
+          title="TEAM MEMBER"
           paragraph="Our Innovative & Visionary Team."
           center
           mb="50px"
