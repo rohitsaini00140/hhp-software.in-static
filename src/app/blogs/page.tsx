5f4b40242a8f6c6e5b608447d805beaf10b01146
rohitsaini00140 <rohit@hhpsoftware.in>
2025-05-19 12:08:48 +0530

    fixed blog page.

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 
 interface BlogPost {
   id: number;
